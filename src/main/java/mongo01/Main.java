0c02c360505c93a23a9e9bdf31809b63679a906c
elarreglador <elarreglador@protonmail.com>
2025-02-05 15:28:20 +0100

    ✨ Cambio menor: Muestra el resultado de una busqueda

--- src/main/java/mongo01/Main.java
+++ src/main/java/mongo01/Main.java
@@ -1,15 +1,21 @@
 package mongo01;
 
+import org.bson.Document;
+
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		// Conectamos a mongodb
+		// Conectamos a mongodb (mongodb://localhost:27017)
 		MongoClient client = MongoClients.create();
 		
 		// Obtenemos lista de nombres de bases de datos
@@ -30,7 +36,28 @@ public class Main {
 		for (String nombreColeccion : colecciones) {
 			System.out.println(" - " + nombreColeccion);
 		}
-
+		
+		// Obtenemos la coleccion peliculas
+		MongoCollection<org.bson.Document> col = db.getCollection("peliculas");
+		
+		// Buscamos Jurassic World
+		String resultado = col.find(Filters.eq("title", "Jurassic World") )
+			.projection( // Define qué campos se incluyen o excluyen en el resultado.
+				Projections.fields(
+					// Solo estos campos en el resultado
+					Projections.include("title", "year", "directors"),
+					// Excluye el campo _id
+					Projections.excludeId()
+				)
+			)
+			.first() // Solo el primero
+			.toJson(); // Convierte el BSON formato JSON.
+		
+		System.out.println("\nResultado busqueda de 'Jurassic World':");
+		System.out.println(resultado);
+		
+		
+		client.close();
 	}
 
 }
