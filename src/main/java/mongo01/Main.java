2743484512e38bb504c9f174aceed7f110f263ea
elarreglador <elarreglador@protonmail.com>
2025-02-05 13:58:54 +0100

    ✨ Cambio menor: Muestra listado de bases de datos, selecciona Mflix y muestra listado de colecciones

--- src/main/java/mongo01/Main.java
+++ src/main/java/mongo01/Main.java
@@ -2,18 +2,33 @@ package mongo01;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		// Conectamos a mongodb
 		MongoClient client = MongoClients.create();
+		
+		// Obtenemos lista de nombres de bases de datos
 		MongoIterable<String> nombresBd = client.listDatabaseNames();
 		
 		System.out.println("BD disponibles en el servidor monbodb:");
-		for (String nombre : nombresBd) {
-			System.out.println(" - " + nombre);
+		for (String nombreBD : nombresBd) {
+			System.out.println(" - " + nombreBD);
+		}
+		
+		// Conectamos a la bd llamada mflix
+		MongoDatabase db = client.getDatabase("mflix");
+		
+		// Obtenemos lista de colecciones (tablas)
+		MongoIterable<String> colecciones = db.listCollectionNames();
+		
+		System.out.println("\nTablas disponibles en " + db.getName());
+		for (String nombreColeccion : colecciones) {
+			System.out.println(" - " + nombreColeccion);
 		}
 
 	}
